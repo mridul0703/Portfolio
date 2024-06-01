f258d62aa1d27c25baf08c9e2ff9cdfdec212e10
Mridul M Kumar <98074317+mridul0703@users.noreply.github.com>
2024-06-01 16:33:30 +0530

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,112 +1,49 @@
-import React, { useState } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-} from "react-icons/ai";
+import React, { useEffect, useState } from 'react';
+import Search from './Search';
+import './Navbar.css'; // Import CSS file
+import logo from '../images/logo.png';
 
-import { CgFileDocument } from "react-icons/cg";
+const Navbar = ({ onCitySelect, initialWeatherData, onTempUnitChange }) => {
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [tempUnit, setTempUnit] = useState('celsius'); // State to hold temperature unit
 
-function NavBar() {
-  const [expand, updateExpanded] = useState(false);
-  const [navColour, updateNavbar] = useState(false);
-
-  function scrollHandler() {
-    if (window.scrollY >= 20) {
-      updateNavbar(true);
-    } else {
-      updateNavbar(false);
+  useEffect(() => {
+    if (initialWeatherData) {
+      setSelectedCity({ name: initialWeatherData.name, country: initialWeatherData.sys.country });
     }
-  }
+  }, [initialWeatherData]);
 
-  window.addEventListener("scroll", scrollHandler);
+  const handleCitySelect = (city) => {
+    setSelectedCity(city);
+    onCitySelect(city); // Pass city data (including coordinates) to the parent component
+  };
+
+  const handleTempUnitChange = (event) => {
+    const selectedUnit = event.target.value;
+    setTempUnit(selectedUnit);
+    onTempUnitChange(selectedUnit); // Pass selected unit to parent component
+  };
 
   return (
-    <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
-      <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/mridul0703/portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <span className="app-name">Weather App</span>
+      </div>
+      <div className="navbar-mid">
+        <Search onCitySelect={handleCitySelect} />
+      </div>
+      <div className="navbar-right">
+        <span className="country-code">
+          {selectedCity ? selectedCity.country : 'NN'}
+        </span>
+        <select className="temp-unit-select" value={tempUnit} onChange={handleTempUnitChange}>
+          <option value="celsius">C</option>
+          <option value="fahrenheit">F</option>
+        </select>
+      </div>
+    </nav>
   );
-}
+};
 
-export default NavBar;
+export default Navbar;
